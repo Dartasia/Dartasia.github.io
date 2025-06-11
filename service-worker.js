fe437972320e1343bad805543db0e02c2ef153eb
Dartasia <alexdepatielaflamme@gmail.com>
2025-06-10 21:57:37 -0400

    Website Creation (Basic File Upload)
    COntent was from my previously hosted website on .io (account was closed due to inactivity, I'm currently uploading the small teest website here).

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const cacheName = 'pwa-cache-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+// Install event: Cache assets for offline use
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => {
+        return cache.addAll(assetsToCache);
+      })
+  );
+});
+
+// Activate event: Clean up old caches if necessary
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch event: Serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
